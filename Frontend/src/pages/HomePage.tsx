49c13174ec8d4bc04187724c7ca07b88d107846d
Norbu-d <02230293.cst@rub.edu.bt>
2025-05-13 14:38:38 +0600

    db connected to internet

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -2,14 +2,16 @@
 import { LeftSidebar } from "../components/Home/LeftSidebar";
 import { MiddleContent } from "../components/Home/MiddleContent";
 import { RightSidebar } from "../components/Home/RightSidebar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen bg-[#121212] text-white">
+    <div className="flex h-screen bg-black text-white overflow-hidden">
       <LeftSidebar />
-      <MiddleContent />
+      <ScrollArea className="w-full max-w-[750px] border-x border-zinc-800"> 
+        <MiddleContent />
+      </ScrollArea>
       <RightSidebar />
     </div>
   );
-
 }
