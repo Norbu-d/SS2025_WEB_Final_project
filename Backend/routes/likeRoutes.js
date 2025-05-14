c83e1eacdec0c07d5c60c64c4ad013216f10dbc6
SoftwareBob12345678910 <02230284.cst@rub.edu.bt>
2025-05-14 14:17:28 +0600

    updating_likeRoutes.js

--- Backend/routes/likeRoutes.js
+++ Backend/routes/likeRoutes.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const router = express.Router();
-// const { likePost, unlikePost, getLikes } = require('../controllers/likeController'); // Destructured import
-// const authMiddleware = require('../middleware/auth');
+const express = require('express');
+const router = express.Router();
+const { likePost, unlikePost, getLikes } = require('../controllers/likeController'); // Destructured import
+const authMiddleware = require('../middleware/auth');
 
-// // Like a post
-// router.post('/posts/:postId/like', authMiddleware, likePost);
+// Like a post
+router.post('/posts/:postId/like', authMiddleware, likePost);
 
-// // Unlike a post
-// router.delete('/posts/:postId/like', authMiddleware, unlikePost);
+// Unlike a post
+router.delete('/posts/:postId/like', authMiddleware, unlikePost);
 
-// // Get post likes
-// router.get('/posts/:postId/likes', getLikes);
+// Get post likes
+router.get('/posts/:postId/likes', getLikes);
 
-// module.exports = router;
+module.exports = router;
